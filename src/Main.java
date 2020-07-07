484d179699763e93a7add84e3182fcb67a7f89c6
JoseAntonioMartinez2525 <63381930+JoseAntonioMartinez2525@users.noreply.github.com>
2020-07-07 11:35:38 -0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,61 +1,55 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		int h,m,s;
 		Scanner teclado = new Scanner(System.in);
-        Tiempo time = new Tiempo();
-        String d;
-        //time.ponerEnHora(24,24,59,59);
-        //System.out.println(time.verHora());
-        System.out.println("Desea cambiar el reloj? ");
-        d=teclado.nextLine();
-        registrar();
-
-    }
-	static Tiempo registrar() {
-		Scanner teclado = new Scanner(System.in);		
-        Tiempo time = new Tiempo();	
-        int h,m,s,d;
-
-        System.out.println("Ingrese el dia: 1.Lunes, 2.martes, 3.miercoles, 4.jueves, 5.viernes, 6.sabado, 7.domingo");       
-		d=teclado.nextInt();
-        String a;
-        
-        System.out.print("hora: ");
-		h=teclado.nextInt();
+		Tiempo hora;
 		
-        System.out.print("minutos: ");
-		m=teclado.nextInt();
 		
-        System.out.print("segundos: ");
-		s=teclado.nextInt();
+		System.out.print("Ingrese las horas: ");
+		h = teclado.nextInt();
 		
-		if(h==24) {
-			switch (d) {
-			case 1: System.out.println("El dia es: martes");break;
-			case 2: System.out.println("El dia es: miercoles");break;
-			case 3: System.out.println("El dia es: jueves");break;
-			case 4: System.out.println("El dia es: viernes");break;
-			case 5: System.out.println("El dia es: sabado");break;
-			case 6: System.out.println("El dia es: domingo");break;
-			case 7: System.out.println("El dia es: lunes");break;
-			default:
-				break;
-			}
-		}
-	       time.ponerEnHora(12,h,m,s); 
-	       
-	       System.out.println(time.verHora());	       
-        time.incrementar();
-        System.out.println(time.verHora());
-        return time;
-        
-	}
-
-
+		System.out.print("Ingrese los minutos: ");
+		m = teclado.nextInt();
 		
-	}
-
+		System.out.print("Ingrese los segundos: ");
+		s = teclado.nextInt();
+		
+		hora = new Tiempo(h,m,s);
+		
+		System.out.println(hora.toString());
+		
+		System.out.println("\n\n<======Aumentando el tiempo=======>");
+		hora.aumentarHoras(2);
+	
+		hora.aumentarMinutos(120);
+		System.out.println(hora.toString());
+		
+		hora.aumentarSegundos(3400);
+		System.out.println(hora.toString());
+		
+		if(h>12)System.out.println(hora.toString()+" am");
+		else System.out.println(hora.toString()+" pm");
+		
+		System.out.println("\n\n<======Decrementando el tiempo=======>");	
+		hora.disminuirHoras(22);
+		System.out.println(hora.toString());
+		
+		hora.disminuirMinutos(120);
+		System.out.println(hora.toString());
+		
+		hora.disminuirMinutos(3600);
+		System.out.println(hora.toString());
+		
+		if(h<12)
+		System.out.println(hora.toString()+" am");
+		else
+		System.out.println(hora.toString()+" pm");
+	}  
+	 
+}
 
